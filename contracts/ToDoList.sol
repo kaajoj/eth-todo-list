5368210b69992e73d72e0d1881a5c2bb50fa79d9
kaajoj <kaajooj@gmail.com>
2019-09-17 02:40:11 +0200

    Index, app and testing

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract ToDoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        creatTask("Check out google.com");
+    }
+
+    function creatTask(string memory _contnet) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _contnet, false);
+    }
+
 }
